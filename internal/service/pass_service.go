1f1e6974d9115fcd37683dbf3d3a1fb55baf21dc
Loi Phan <phanngocloi2358@gmail.com>
2026-03-02 15:42:49 +0700

    update services
    update services

---
+++ internal/service/pass_service.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"bytes"
+	"crypto/rand"
+	"math/big"
+)
+
+const (
+	charset        = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	passwordLength = 10
+)
+
+type passwordService struct {
+}
+
+// Password  interface represents the password services interface
+type Password interface {
+}
+
+// NewPassWordService creates a new password service
+func NewPassWordService() Password {
+	return &passwordService{}
+}
+
+func (s *passwordService) GeneratePassword() (string, error) {
+	var strBuilder bytes.Buffer
+
+	for i := 0; i < passwordLength; i++ {
+		randomIndex, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return "", err
+		}
+		strBuilder.WriteByte(charset[randomIndex.Int64()])
+	}
+	return strBuilder.String(), nil
+
+}
